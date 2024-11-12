d18ebe9fa8ec954c5ee214cb7e64b85df539e28c
Luiza Ribeiro <ribeiroluiza.jlle@gmail.com>
2024-11-11 23:33:11 -0300

    ajuste

--- produtos/models.py
+++ produtos/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=0)
     
     def get_total_price(self):
         return self.quantity * self.product.price
